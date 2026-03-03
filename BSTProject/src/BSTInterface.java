997ef89b1ed79c7ad97f8ce4a74037f03ba50a43
KANDRADE7 <andradekate@icloud.com>
2026-03-03 03:55:11 +0000

    traversals added

--- BSTProject/src/BSTInterface.java
+++ BSTProject/src/BSTInterface.java
@@ -35,13 +35,52 @@ public interface BSTInterface
 	public boolean delete(Comparable old);
 	// this method deletes the object being passed to the method from the tree.  When you remove the node you must then change any children around to accommodate for the deleted node.  Return true if the node was successfully deleted, false if it was not in the tree and therefore not deleted.
 
-	public void printInOrder();
+	public void printInOrder(){
 	// prints the tree using an In Order traversal - recursion
+		if(root!=null){
+			printInHelper(root.getLeft());
+			System.out.print(root.getValue());
+			printInHelper(root.getRight());
+		}//end if
+	private void printInHelper(TreeNode subroot){
+		if(subroot!= null){
+			printInHelper(subroot.getLeft());
+			System.out.print(subroot.getValue());
+			printInHelper(subroot.getRight());
+		}//end if
+	}//end helper
+	}//end inorder traversal 
 	
 
-	public void printPreOrder();
-	// prints the tree using a Pre Order traversal - recursion
-
-	public void printPostOrder();
+	public void printPreOrder(){
+		// prints the tree using a Pre Order traversal - recursion
+		if(root!=null){
+			System.out.println(root.getValue());
+			printPreHelper(root.getLeft()); 
+			printPreHelper(root.getRight());
+		}//end if
+		private void printPreHelper(TreeNode subroot){
+			if(subroot!=null){
+				System.out.println(subroot.getValue());
+				printPreHelper(subroot.getLeft());
+				printPreHelper(subroot.getRight());
+			}//end if
+		}//end helper
+	}//end preOrder traversal 
+	
+	public void printPostOrder(){
 	// prints the tree using a Post Order traversal – recursion
-}
+		if(root!= null){
+			printPostHelper(root.getLeft());
+			printPostHelper(root.getRight());
+			System.out.print(root.getValue());
+		}//end if
+		private void printPostHelper(TreeNode subroot){
+			if(subroot!=null){
+				printPostHelper(subroot.getLeft());
+				printPostHelper(subroot.getRight());
+				System.out.print(subroot.getValue());
+			}//end if
+		}//end helper
+	}//end postorder traversal
+}//end interface
