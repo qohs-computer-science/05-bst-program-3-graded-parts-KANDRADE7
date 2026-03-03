41fecdfdd93fa65130e5ec8f2908907e75c8be8e
KANDRADE7 <andradekate@icloud.com>
2026-03-03 04:10:35 +0000

    day 1

--- BSTProject/src/BST.java
+++ BSTProject/src/BST.java
@@ -1,6 +1,10 @@
 import java.lang.Comparable;
 
-public class BST implements BSTInterface
-{
-
-}
+public class BST implements BSTInterface{
+    private TreeNode root;
+    private int size;
+      public BST(){
+        root = null;
+        size =0;
+    }//end constructor
+}//end class
--- BSTProject/src/BSTTester.java
+++ BSTProject/src/BSTTester.java
@@ -9,11 +9,35 @@ Binary search tree
 
  public class BSTTester {
    public static void main(String[] args) {
-   private TreeNode rootNode;
-      public BST(){
-        root = null;
-    }//end constructor
- }//end main
-  
+    BST tree = new BST(); //creates tree
+    //test add method
+    tree.add("O");
+    tree.add("R");
+    tree.add("C");
+    tree.add("H");
+    tree.add("A");
+    tree.add("R");
+    tree.add("D");
+    tree.add("Q");
+    tree.add("U");
+    tree.add("I");
+    tree.add("N");
+    tree.add("C");
+    tree.add("E");
 
+    //test all traversals
+    //inorder
+    System.out.println("In order: ");
+    tree.printInOrder();
+
+     //pre order
+    System.out.println("Pre order: ");
+    tree.printPreOrder();
+
+     //post order
+    System.out.println("Post order: ");
+    tree.printPostOrder();
+
+    
+ }//end main
  }//end BSTTester
